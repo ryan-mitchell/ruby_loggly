03164f731c2db6e1c044727004fd2393c7993472
Ryan Mitchell <mitchellryanj@gmail.com>
2011-07-28 17:06:03 -0700

    A few changes

--- lib/ruby_loggly.rb
+++ lib/ruby_loggly.rb
@@ -8,6 +8,10 @@ class Loggly
 		@pass = password
 	end
 
+	def log(log_string)
+		RestClient.post('https://logs.loggly.com/inputs/faa5af5f-c6d1-43fd-bda8-7b49a4ef1f58', log_string)
+	end
+
 	def search(query)
 		RestClient.get(@uri, {:params => {:q => query}})
 	end
--- spec/ruby_loggly_spec.rb
+++ spec/ruby_loggly_spec.rb
@@ -1,18 +1,47 @@
 require 'spec_helper'
 
-describe Loggly, "#search" do
+describe Loggly do
 
-	before(:each) do
+	before(:all) do
 		@test_uri = 'example.loggly.com'
 		@test_user = 'user'
 		@test_pass = 'pass'
-		@loggly = Loggly.new(@test_uri, @test_user, @test_pass)
-		RestClient.stub!(:get)
+		@test_key = '123456'
+		@loggly = Loggly.new(
+			:uri => @test_uri, 
+			:user => @test_user, 
+			:pass => @test_pass, 
+			:key => @test_key
+		)
 	end
 
-	it "should generate an appropriate RestClient get request" do
-		search_string = "search string"
-		RestClient.should_receive(:get).with(@test_uri, {:params => {:q => search_string}})
-		@loggly.search("search string")
+	it "should throw an exception if no URI is passed" do
+
+	end	
+
+	describe "#search" do
+
+		before(:each) do
+			RestClient.stub!(:get)
+		end
+
+		it "should generate an appropriate RestClient get request" do
+			search_string = "search string"
+			RestClient.should_receive(:get).with(@test_uri, {:params => {:q => search_string}})
+			@loggly.search("search string")
+		end
 	end
+
+	describe "#log" do
+		
+		before(:each) do
+			RestClient.stub!(:post)
+		end
+
+		it "should generate an appropriate RestClient POST" do
+			RestClient.should_receive(:post)
+			@loggly.log('this is a log')
+		end
+	end
+
 end
