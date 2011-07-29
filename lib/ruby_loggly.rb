c0436501df41f8e65bd45fe4aa0b0d1848aa62ec
Ryan Mitchell <mitchellryanj@gmail.com>
2011-07-29 09:06:13 -0700

    Retabbing

--- lib/ruby_loggly.rb
+++ lib/ruby_loggly.rb
@@ -2,19 +2,19 @@ require 'rest_client'
 
 class Loggly
 
-	def initialize(options)
-		@subdomain = options[:subdomain]
-		@user = options[:user]
-		@pass = options[:pass]
-		@key = options[:key]
-	end
+  def initialize(options)
+    @subdomain = options[:subdomain]
+    @user = options[:user]
+    @pass = options[:pass]
+    @key = options[:key]
+  end
 
-	def log(log_data)
-		RestClient.post("https://logs.loggly.com/inputs/#{@key}", log_data)
-	end
+  def log(log_data)
+    RestClient.post("https://logs.loggly.com/inputs/#{@key}", log_data)
+  end
 
-	def search(query)
-		RestClient.get("https://#{@user}:#{@pass}@#{@subdomain}.loggly.com/api/search", {:params => {:q => query}})
-	end
+  def search(query)
+    RestClient.get("https://#{@user}:#{@pass}@#{@subdomain}.loggly.com/api/search", {:params => {:q => query}})
+  end
 
 end
--- spec/ruby_loggly_spec.rb
+++ spec/ruby_loggly_spec.rb
@@ -2,57 +2,57 @@ require 'spec_helper'
 
 describe Loggly do
 
-	before(:all) do
-		@test_subdomain = 'example'
-		@test_user = 'user'
-		@test_pass = 'pass'
-		@test_key = '123456'
-		@loggly = Loggly.new(
-			:subdomain => @test_subdomain, 
-			:user => @test_user, 
-			:pass => @test_pass, 
-			:key => @test_key
-		)
-	end
+  before(:all) do
+    @test_subdomain = 'example'
+    @test_user = 'user'
+    @test_pass = 'pass'
+    @test_key = '123456'
+    @loggly = Loggly.new(
+      :subdomain => @test_subdomain, 
+      :user => @test_user, 
+      :pass => @test_pass, 
+      :key => @test_key
+    )
+  end
 
-	describe "#search" do
+  describe "#search" do
 
-		before(:each) do
-			RestClient.stub!(:get)
-		end
+    before(:each) do
+      RestClient.stub!(:get)
+    end
 
-		it "should generate an appropriate RestClient GET" do
-			search_string = "search string"
-			RestClient.should_receive(:get).with(
+    it "should generate an appropriate RestClient GET" do
+      search_string = "search string"
+      RestClient.should_receive(:get).with(
         "https://#{@test_user}:#{@test_pass}@#{@test_subdomain}.loggly.com/api/search", 
         {:params => {:q => search_string}}
       )
-			@loggly.search(search_string)
-		end
-	end
+      @loggly.search(search_string)
+    end
+  end
 
-	describe "#log" do
-		
-		before(:each) do
-			RestClient.stub!(:post)
-		end
+  describe "#log" do
+    
+    before(:each) do
+      RestClient.stub!(:post)
+    end
 
-		it "should generate an appropriate RestClient POST with a raw payload" do
-			log_string = "this is a test log"
-			RestClient.should_receive(:post).with(
-				"https://logs.loggly.com/inputs/%s" % @test_key,
-				log_string
-			)
-			@loggly.log(log_string)
-		end
+    it "should generate an appropriate RestClient POST with a raw payload" do
+      log_string = "this is a test log"
+      RestClient.should_receive(:post).with(
+        "https://logs.loggly.com/inputs/%s" % @test_key,
+        log_string
+      )
+      @loggly.log(log_string)
+    end
 
-		it "should generate an appropriate RestClient POST with parameters" do
-			log_parameters = { :time => 123456, :type => 'foo', :logger => 'bar' }
-			RestClient.should_receive(:post).with(
-				"https://logs.loggly.com/inputs/%s" % @test_key,
-				log_parameters
-			)
-			@loggly.log(log_parameters)
-		end
-	end
+    it "should generate an appropriate RestClient POST with parameters" do
+      log_parameters = { :time => 123456, :type => 'foo', :logger => 'bar' }
+      RestClient.should_receive(:post).with(
+        "https://logs.loggly.com/inputs/%s" % @test_key,
+        log_parameters
+      )
+      @loggly.log(log_parameters)
+    end
+  end
 end
